f914cc9d6d15a81281687775bd68b73bda331500
Vadim <leaveplace01@gmail.com>
2024-12-18 13:48:01 +0400

    Update update-service.bat

--- update-service.bat
+++ update-service.bat
@@ -1,1 +1,8 @@
-set "GITHUB_URL=
+setlocal
+set "GITHUB_URL=https://github.com/Cross-devspec/TMDR/blob/main/.service/version.txt"
+set "CURRENT_VERSION=1.0.0"
+set "VERSION_FILE=version.txt"
+
+for /f "delims=" %%A in ('powershell -command "[datetime]::Now.ToString('yyyy-MM-dd HH:mm:ss')"') do set CURRENT_TIMESTAMP=%%A
+
+endlocal
