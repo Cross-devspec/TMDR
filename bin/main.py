891d26e919cbab18426ec7e2a468faff11a87632
Vadim <leaveplace01@gmail.com>
2024-12-18 10:56:43 +0400

    Uhm...

---
+++ bin/main.py
@@ -0,0 +1,14 @@
+import random
+import os
+import rich
+
+from time import sleep
+
+from rich.console import Console
+from rich.table import table
+# Инициал консоли
+console = Console()
+# Инициал таблицы
+table = Table()
+
+print("Какой скрипт установить как службу?")
